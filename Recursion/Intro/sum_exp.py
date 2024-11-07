43483fcdd55cc1a49b0f6e08a0b30f7fcb07cb49
AlgoWebBot <shimulzahan636@gmail.com>
2024-11-08 00:46:23 +0600

    add a sum example using recursion

--- Recursion/Intro/sum_exp.py
+++ Recursion/Intro/sum_exp.py
@@ -3,7 +3,31 @@ def sum(n):
     # base case
     if n== 0:
         return 0
-    
+    # recursion call
     return n + sum(n - 1)
 
 print(sum(5))
+
+# Factorial
+
+def factor(n):
+    # base case
+    if n==0:
+        return 1
+    # recursion call
+    return n * factor(n-1)
+
+print(factor(4))
+
+
+# Factorial using tail recursion but not using backtracking
+
+def factor(n, accumulator):
+
+    # base case
+    if n==0:
+        return accumulator
+    # recursion call
+    return factor(n - 1, n*accumulator)
+
+print(factor(10, 1))
---
+++ Recursion/Intro/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+def factor(n):
+#     # base case
+#     if n==0:
+#         return 1
+#     # recursion call
+#     return n * factor(n-1)
+
+# print(factor(4))
